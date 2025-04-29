690c20dd01fba5ee37faca94b8110cdbffe8a2eb
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-04-29 11:03:55 +0500

    first commit

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+import Places from "./Places.jsx";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [AvailablePlaces, setAvailablePlaces] = useState([]);
+
+  return (
+    <Places
+      title="Available Places"
+      places={[]}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
