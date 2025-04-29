2409e743e9f70abb5273714c66b97d4b0880d8cc
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-04-29 12:17:18 +0500

    error state is implemented

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,27 +1,39 @@
 import { useEffect, useState } from "react";
 
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(true);
+  const [isError, setIsError] = useState();
 
   useEffect(() => {
     setIsFetching(true);
 
     async function fetchPlaces() {
-      const response = await fetch("http://localhost:3000/places");
-      console.log(response);
-      const responseData = await response.json();
-      console.log(responseData);
+      try {
+        const response = await fetch("http://localhost:3000/placess");
+        const responseData = await response.json();
 
-      setAvailablePlaces(responseData.places);
+        if (!response.ok) {
+          throw new Error("an error occured, try again later");
+        }
+
+        setAvailablePlaces(responseData.places);
+      } catch (error) {
+        setIsError({ message: "Please try again later" });
+      }
 
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (isError) {
+    return <Error title="Error Occured" message={isError.message} />;
+  }
 
   return (
     <Places
