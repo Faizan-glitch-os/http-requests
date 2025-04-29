ed499faf359c28234bb668c7821e09e78979b0a0
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-04-29 14:43:08 +0500

    function is written in http.js file to send user picked places

--- src/http.js
+++ src/http.js
@@ -1,4 +1,4 @@
-export default async function fetchAvailablePlaces() {
+export async function fetchAvailablePlaces() {
   const response = await fetch("http://localhost:3000/places");
   const responseData = await response.json();
 
@@ -7,4 +7,20 @@ export default async function fetchAvailablePlaces() {
   }
 
   return responseData.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const responseData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("User data was not sent...");
+  }
+
+  return responseData.message;
 }
