25f6e94fb92ce126a2fa122f3774606767f3760b
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-04-29 11:47:58 +0500

    loading state during data fetching is implemented

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,13 +4,20 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
+    setIsFetching(true);
+
     async function fetchPlaces() {
       const response = await fetch("http://localhost:3000/places");
+      console.log(response);
       const responseData = await response.json();
+      console.log(responseData);
 
       setAvailablePlaces(responseData.places);
+
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -20,6 +27,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Currently loading data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
